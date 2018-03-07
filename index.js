780ef495e38ea4b7db0a2149df9c19cfb5e6b255
Nam <jimmy180983@gmail.com>
2018-03-07 21:47:15 +0700

    push

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require('path');
 const jsonParser = require('body-parser').json();
 const routes = require('./routes/index.js');
 const app = express();
-app.use(session({
-    secret: 'jsdf7389isacuy28',
-    resave: false,
-    saveUninitialized: true,
-    cookie:{
-      maxAge:60*60*100*24
-      }
-}));
+// app.use(session({
+//     secret: 'jsdf7389isacuy28',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie:{
+//       maxAge:60*60*100*24
+//       }
+// }));
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.listen(process.env.PORT || 5000);
 app.use('/', routes);
 
